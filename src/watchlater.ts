ccc4ee52d698f6dde5e5c64120ccfd2275c79205
Wesley Sykes <terrysawgames@gmail.com>
2026-02-03 19:50:29 -0500

    Further setup and basic youtube navigation

--- src/watchlater.ts
+++ src/watchlater.ts
@@ -1,13 +1,26 @@
-function start()
-{
-	let observer : MutationObserver = new MutationObserver(() => {
-		let message : string | number = "Hello, TypeScript"
 
-		console.log(message)
-	})
-
-	observer.observe(document.body, { childList: true, subtree: true })
-}
-
-
-start()
+document.addEventListener('yt-navigate-finish', () => {
+  console.log('YouTube navigation finished');
+  if (window.location.href.endsWith("playlist?list=WL"))
+  {
+	setTimeout(()=>{}, 5000)
+  	let playlist_videos : HTMLElement | null = document.querySelector("ytd-playlist-video-list-renderer>#contents")
+  	if (playlist_videos)
+  	{
+		let videos : HTMLCollection = playlist_videos.children
+		console.log(videos.length)
+  		for (var i = 0; i < videos.length; i++)
+  		{
+			let video = videos[i];
+			let button : HTMLButtonElement | null = video?.querySelector("#menu>ytd-menu-renderer>yt-icon-button")
+			button?.click()
+  			console.log(video)
+			break;
+  		}
+  	}
+  	else
+  	{
+  		alert("Couldn't find playlsit videos")
+  	}
+  }
+})
